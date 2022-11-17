7de784082fc968f7b86c6293c61c3949e11b81b2
hanle <hanle@DESKTOP-FFHDDQQ.dhcp.wustl.edu>
2022-11-17 13:50:28 -0600

    assignment 8 time

--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,9 +1,61 @@
 package studio8;
 
+import java.util.Objects;
 
 public class Time {
+	private int hour;
+	private int minute;
+	private boolean is24Hr;
+	
+
+	public Time(int hour, int minute, boolean is24Hr) {
+		super();
+		this.hour = hour;
+		this.minute = minute;
+		this.is24Hr = is24Hr;
+	}
+	
+	
+
+
+	@Override
+	public String toString() {
+		return hour+ ":" + minute;
+	}
+
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
+
+
 
 	public static void main(String[] args) {
+		Time rightNow = new Time(13, 47, true);
+		System.out.println(rightNow);
+		Time minuteAgo= new Time(13,47,true);
+		System.out.println(minuteAgo);
+		System.out.println(rightNow.equals(minuteAgo));
+		
  
     	
     }
